497ee0d54d058e9f26c9095302dd75c94501461c
Wellington Nascente Hirsch <wnhirsch@inf.ufrgs.br>
2020-10-04 02:39:17 -0300

    Project and first files created

---
+++ source/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    // Inicializa cenário do jogo
+    // Laço de jogo
+    // Contabiliza pontuação
+    // Carrega score
+    // Ordena e mostra score do jogo
+    // Encerra
+
+    return 0;
+}
